48fc4f7c673cc4a85a97e292028868c788e11217
Brad Dunlap <braddunlapdpt@gmail.com>
2023-05-23 13:46:36 -0400

    feat: calculates playtime in minutes on playlist index page

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,12 +1,13 @@
 class PlaylistsController < ApplicationController
   def index
     @playlists = Playlist.all
+    @playlist_time_minutes = calculate_playlist_times
   end
 
   def show
     @playlist = Playlist.find(params[:id])
     @songs_in_playlist = @playlist.songs
-    @playlist_time_in_minutes = playlist_time_in_minutes
+    @playlist_time_in_minutes = playlist_time_in_minutes(@playlist)
   end
 
   def new
@@ -15,7 +16,27 @@ class PlaylistsController < ApplicationController
 
   def create; end
 
-  def playlist_time_in_minutes
-    #TODO: implement
+  def playlist_time_in_minutes(playlist)
+		total_sec = 0
+	
+		playlist.songs.each do |song|
+			min, sec = song.length.split(':')
+			total_sec += (min.to_i * 60) + sec.to_i
+		end
+	
+		total_min = (total_sec / 60.0).round
+	
+		"#{total_min} minutes"
+	end
+	
+
+  def calculate_playlist_times
+    playlist_times = {}
+    
+    @playlists.each do |playlist|
+      playlist_times[playlist.id] = playlist_time_in_minutes(playlist)
+    end
+
+    playlist_times
   end
 end
