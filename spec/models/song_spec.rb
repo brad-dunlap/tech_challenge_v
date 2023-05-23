57ec82b9fc493ae9d2c83210eb26402782e1bfe7
Brad Dunlap <braddunlapdpt@gmail.com>
2023-05-23 12:39:34 -0400

    updated testing for new associations for artist

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -10,20 +10,21 @@ RSpec.describe Song, type: :model do
   end
 
   describe 'associations' do
-		it { should belong_to(:artist) }
+    it { should belong_to(:artist) }  # Add association test for artist
     it { should have_many(:playlist_tracks) }
     it { should have_many(:playlists).through(:playlist_tracks) }
   end
 
-	describe 'class methods' do
-		it 'should return the longest song' do
-			short = create(:song, length: 200)
-			med = create(:song, length: 250)
-			long = create(:song, length: 300)
+  describe 'class methods' do
+    it 'should return the longest song' do
+      artist = create(:artist)  # Create an artist for the song
+      short = create(:song, length: 200, artist: artist)  # Assign the artist to the song
+      med = create(:song, length: 250, artist: artist)
+      long = create(:song, length: 300, artist: artist)
 
-			result = Song.get_longest_song
-			
-			expect(result).to eq(long)
-		end
-	end
+      result = Song.get_longest_song
+
+      expect(result).to eq(long)
+    end
+  end
 end
